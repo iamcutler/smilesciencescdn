767de37b5e30cb77a947072b41da5ff02de203a0
Allan Cutler <musicfan03@gmail.com>
2013-06-20 00:53:51 -0700

    Introduced rails application

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Smilecdn::Application.config.session_store :cookie_store, :key => '_smilecdn_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Smilecdn::Application.config.session_store :active_record_store
