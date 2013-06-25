23e36aab6fa7e76bfa0005d532a64192b3b13080
Allan Cutler <musicfan03@gmail.com>
2013-06-25 11:29:18 -0700

    Introduced export action for the redeem controller

--- app/controllers/redeem_controller.rb
+++ app/controllers/redeem_controller.rb
@@ -1,4 +1,7 @@
 class RedeemController < ApplicationController
   def index
   end
+
+  def export
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -63,4 +63,5 @@ Smilecdn::Application.routes.draw do
 
   #Voucher redemptions
   match '/redeem' => "redeem#index", :as => :redeem
+  match '/redeem/export/:start/:end' => "redeem#export"
 end
